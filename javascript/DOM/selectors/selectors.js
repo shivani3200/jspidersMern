b60fab4f5452f0f30b5e8d29fe9f7e405ee07065
shivani3200 <shivani13222@gmail.com>
2026-02-21 14:43:57 +0530

    DOM- selectors

---
+++ javascript/DOM/selectors/selectors.js
@@ -0,0 +1,32 @@
+// getElementsByTagName()
+var a = document.getElementsByTagName('li');
+// console.log(a)
+
+
+// getElementsByClassName()
+var b = document.getElementsByClassName('hero');
+// console.log(b)
+
+
+// getElementsById()
+var c = document.getElementById('hero');
+// console.log(c);
+
+
+// querySelector()
+var d = document.querySelector('.hero')
+// console.log(d)
+var e = document.querySelector('li')
+// console.log(e)
+var f = document.querySelector('#hero')
+// console.log(f)
+
+
+
+// querySelectorAll()
+var g = document.querySelectorAll('.hero')
+console.log(g)
+var h = document.querySelectorAll('li')
+console.log(h)
+var i = document.querySelectorAll('#hero')
+console.log(i)
