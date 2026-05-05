a2d4f9f70496112cacf7867c9259232057c378c9
Shivani Kumari <shivani13222@gmail.com>
2026-05-05 17:53:54 +0530

    added comments to operator and variable file

--- javascript/variables/variable.js
+++ javascript/variables/variable.js
@@ -1,39 +1,39 @@
-console.log("variable")
+console.log("variable - container to store any value")
 function main(){
     let n = 10;
-    console.log(n, typeof n);
+    console.log(n, typeof n);//number
      n = Infinity;
      n = -Infinity;
      n = NaN;
 
-     let str = 'jsp'; 
+     let str = 'jsp'; //string
      str = "jsp";
      str = `jsp`;
 
-     let bool = true;
+     let bool = true;//boolean
      bool = false;
 
-     let x = undefined;
+     let x = undefined;//undefined
      console.log(typeof x)
      
-     let y = null;
+     let y = null;//null
      console.log(typeof y);
 
-     let z = 234567890987653456789098765n;
+     let z = 234567890987653456789098765n;//BigInt
      let za = BigInt("2345609856787665123456789098765432456789");
 
 
-    //  identifiers - names given by developers to any name-(fn,var,class etc)
+    //  identifiers - names given by developers to any function, variable, class etc
+    let num$1;
+    let $num1;
     let num_1;
     let num1;
-    // let 1num;
+    // let 1num; //wrong identifiers
     // let num 1;
     // let num-1;
     // let num&1;
     // let num#1;
     // let num@1;
-    let num$1;
-    let $num1;
     // let try;
     // let catch;
     // let if;
