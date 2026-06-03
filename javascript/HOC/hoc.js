24666f291499d3e0853d2601394d60bf6b6aa55b
Shivani Kumari <shivani13222@gmail.com>
2026-06-04 00:59:15 +0530

    event props

--- javascript/HOC/hoc.js
+++ javascript/HOC/hoc.js
@@ -8,3 +8,15 @@ function sum(a,b){
 function HOC(a,b,sum){
     return sum(a,b);
 }
+
+
+
+//
+console.log(a); //5
+var a ;
+console.log(a); //undefined
+
+
+console.log(b); //ReferenceError: x is not defined
+let b;
+console.log(b); //undefined
